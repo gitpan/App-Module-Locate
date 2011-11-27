16b1413b6d6a961ed759f6e9bc2aa8619a546445
celogeek <me@celogeek.com>
2011-11-27 13:01:49 +0000

    Import of CELOGEEK/App-Module-Locate-0.1 from CPAN.
    gitpan-cpan-distribution: App-Module-Locate
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         CELOGEEK/App-Module-Locate-0.1.tar.gz
    gitpan-cpan-author:       CELOGEEK
    gitpan-cpan-maturity:     released

---
+++ lib/App/Module/Locate.pm
@@ -0,0 +1,40 @@
+#
+# This file is part of App-Module-Locate
+#
+# This software is copyright (c) 2011 by Geistteufel <geistteufel@celogeek.fr>.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
+package App::Module::Locate;
+
+# ABSTRACT: Interface for module locate
+use strict;
+use warnings;
+our $VERSION = '0.1';    # VERSION
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::Module::Locate - Interface for module locate
+
+=head1 VERSION
+
+version 0.1
+
+=head1 AUTHOR
+
+Geistteufel <geistteufel@celogeek.fr>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Geistteufel <geistteufel@celogeek.fr>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
